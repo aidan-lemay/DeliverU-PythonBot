5fb58d45f8dd958c76d9f3dc60071b422e6225b2
Aidan LeMay <aidanpathways12@gmail.com>
2022-05-28 00:34:19 -0400

    Adding Functions to Remove Reaction After Clock In / Out

--- bot.py
+++ bot.py
@@ -8,37 +8,66 @@ import datetime
 # Bot Setup
 intents = discord.Intents.default()
 bot = commands.Bot(command_prefix="/", intents=intents)
+message = None
 
 # MongoDB Setup
 CONNECTION_STRING = storage.connection
 client = MongoClient(CONNECTION_STRING)
 dbname = client.get_database()
-collection_name = dbname['clockedIn']
+clock_collection = dbname['clockedIn']
+# time_collection = dbname['timeWorked']
 
 async def clockIn(user):
-    data = DataFrame(collection_name.find({'user_id': user.id}))
-    
-    if data['clockedIn'].bool() == True:
+    data = DataFrame(clock_collection.find({'user_id': user.id}))
+    if data.empty:
+        userDoc = {
+            'user_id': user.id,
+            'user_name': user.name,
+            'user_discriminator': user.discriminator,
+            'clockedIn': True,
+            'in_time': datetime.datetime.now(datetime.timezone.utc),
+            'out_time': ""
+        }
+        clock_collection.insert_one(userDoc)
+        dm = await bot.fetch_user(user.id)
+        await dm.send("You Have Been Clocked In!")
+    elif data['clockedIn'].bool() == True:
         dm = await bot.fetch_user(user.id)
         await dm.send("You Have Already Clocked In Today")
+    elif data['clockedIn'].bool() == False:
+        clock_collection.update_one({'user_id': user.id},{'$set':{'clockedIn': True, 'in_time': datetime.datetime.now(datetime.timezone.utc), 'out_time': ''}})
+        dm = await bot.fetch_user(user.id)
+        await dm.send("You Have Been Clocked In!")
     else:
         userDoc = {
             'user_id': user.id,
             'user_name': user.name,
             'user_discriminator': user.discriminator,
             'clockedIn': True,
-            'in_time': datetime.datetime.now(),
+            'in_time': datetime.datetime.now(datetime.timezone.utc),
             'out_time': ""
         }
-        collection_name.insert_one(userDoc)
+        clock_collection.insert_one(userDoc)
         dm = await bot.fetch_user(user.id)
         await dm.send("You Have Been Clocked In!")
 
 async def clockOut(user):
-    data = DataFrame(collection_name.find({'user_id': user.id}))
+    # ToDo: When clocking out, record hours worked into seperate data table | clocked_out - clocked_in = timeWorked
+    data = DataFrame(clock_collection.find({'user_id': user.id}))
     
     if data['clockedIn'].bool() == True:
-        collection_name.update_one({'user_id': user.id},{'$set':{'clockedIn': False, 'out_time': datetime.datetime.now()}})
+        clock_collection.update_one({'user_id': user.id},{'$set':{'clockedIn': False, 'out_time': datetime.datetime.now(datetime.timezone.utc)}})
+        # data = DataFrame(clock_collection.find({'user_id': user.id}))
+        # timeWorked = (data['out_time'] - data['in_time'])
+        # print(timeWorked.seconds())
+        # userTime = {
+        #     'user_id': user.id,
+        #     'user_name': user.name,
+        #     'user_discriminator': user.discriminator,
+        #     'timeWorked': timeWorked,
+        #     'timeRecorded': datetime.datetime.now(datetime.timezone.utc)
+        # }
+        # time_collection.insert_one(userTime)
         dm = await bot.fetch_user(user.id)
         await dm.send("You Have Been Clocked Out!")
     else:
@@ -58,11 +87,9 @@ async def on_reaction_add(reaction, user):
     if not user.bot:
         if reaction.emoji == '✅':
             await clockIn(user)
+            await reaction.remove(user)
         elif reaction.emoji == '❎':
             await clockOut(user)
-
-# @bot.command()
-# async def test(ctx):
-#     await ctx.send("Test!")
+            await reaction.remove(user)
 
 bot.run(storage.token)
