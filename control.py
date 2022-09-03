42ca47970cf796829c910e0e63aadcf3347da6c5
Aidan LeMay <aidanpathways12@gmail.com>
2022-09-02 21:54:45 -0400

    Fixing concat issues...?

--- control.py
+++ control.py
@@ -215,11 +215,11 @@ async def on_reaction_add(reaction, user):
                                             order_collection.update_one({'_id': ObjectId(mid)}, {'$set':{'dasherAssigned': True, 'acceptTime': datetime.datetime.now(datetime.timezone.utc), 'dasherID': user.id}})
                                             order = DataFrame(order_collection.find({'_id': ObjectId(mid)}))
                                             
-                                            smessage = await dm.send(mid + " Order has been accepted!\nPick Up From: " + order.loc[0]['diningAddress'] + "\nDeliver To: " + order.loc[0]['deliveryAddress'] + "\nCustomer Name: " + order.loc[0]['customerName'] + "\nCustomer Phone Number: " + str(order.loc[0]['customerPhone']) + "\nCustomer Order Instructions: " + order.loc[0]['customerInstructions'] + "\nReact with :white_check_mark: to mark as complete!")
+                                            smessage = await dm.send(mid + " Order has been accepted!\nPick Up From: " + str(order.loc[0]['diningAddress']) + "\nDeliver To: " + str(order.loc[0]['deliveryAddress']) + "\nCustomer Name: " + str(order.loc[0]['customerName']) + "\nCustomer Phone Number: " + str(order.loc[0]['customerPhone']) + "\nCustomer Order Instructions: " + str(order.loc[0]['customerInstructions']) + "\nReact with :white_check_mark: to mark as complete!")
                                             await smessage.add_reaction(u"\u2705")
 
                                             channel = bot.get_channel(location['control-channel'])
-                                            await channel.send("Order has been accepted by " + user.name + " at " + str(datetime.datetime.now(datetime.timezone.utc)) + "\nPick Up From: " + order.loc[0]['diningAddress'] + "\nDeliver To: " + order.loc[0]['deliveryAddress'] + "\nCustomer Name: " + order.loc[0]['customerName'] + "\nCustomer Phone Number: " + str(order.loc[0]['customerPhone']) + "\nCustomer Order Instructions: " + order.loc[0]['customerInstructions'])
+                                            await channel.send("Order has been accepted by " + str(user.name) + " at " + str(datetime.datetime.now(datetime.timezone.utc)) + "\nPick Up From: " + str(order.loc[0]['diningAddress']) + "\nDeliver To: " + str(order.loc[0]['deliveryAddress']) + "\nCustomer Name: " + str(order.loc[0]['customerName']) + "\nCustomer Phone Number: " + str(order.loc[0]['customerPhone']) + "\nCustomer Order Instructions: " + str(order.loc[0]['customerInstructions']))
                                             await reaction.message.delete()
                                         else:
                                             await dm.send("You are not clocked in - please clock in before accepting orders.")
@@ -239,7 +239,7 @@ async def on_reaction_add(reaction, user):
                     mid = reaction.message.content.split()[0]
                     order_collection.update_one({'_id': ObjectId(mid)}, {'$set':{'orderComplete': True, 'completeTime': datetime.datetime.now(datetime.timezone.utc)}})
                     channel = bot.get_channel(control)
-                    await channel.send("Order " + mid + " has been completed by " + user.name + " at " + str(datetime.datetime.now(datetime.timezone.utc)))
+                    await channel.send("Order " + str(mid) + " has been completed by " + str(user.name) + " at " + str(datetime.datetime.now(datetime.timezone.utc)))
                     await reaction.message.edit(content="Your Order Has Been Completed! Nice Job!")
 
 @bot.event
